3e9aa43808216a626f6d14ab47db8c9adef180a5
LaurenE <Lauren.salitan@gmail.com>
2024-11-05 13:09:16 -0600

    updated config.js

--- config.js
+++ config.js
@@ -10,12 +10,14 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
+// function getDatabaseUri() {
+//   return (process.env.NODE_ENV === "test")
+//       ? "jobly_test"
+//       : process.env.DATABASE_URL || "jobly";
+// }
 function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+  return process.env.DATABASE_URL || "postgresql://localhost/jobly";
 }
-
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 //
 // WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
